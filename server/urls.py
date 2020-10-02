81d69cb767a234c156e6f2a92908e8cde05e5c78
Flávio Filipe <flaviofilipe.dev@gmail.com>
2020-10-02 02:53:00 -0300

    :memo: Add drf-yasg documentation
    Downgrade DRF (new release is incompative) and add drf-yasg.

--- server/urls.py
+++ server/urls.py
@@ -17,7 +17,28 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="BSI Api Documentation",
+        default_version='v1',
+        description="Api endpoints",
+        # terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="flaviofilipe.dev@gmail.com"),
+        # license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('', include(('blog.urls', 'blog'), namespace='blog')),
     path('admin/', admin.site.urls),
+    path('', schema_view.with_ui('swagger',
+                                 cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc', schema_view.with_ui('redoc',
+                                      cache_timeout=0), name='schema-redoc'),
 ]
