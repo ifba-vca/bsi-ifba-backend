89bb47d65735a347f45cc2c93293f0c717fc9fa1
Flavio Filipe <flaviofilipe.dev@gmail.com>
2021-03-03 20:06:24 -0300

    Add skills endpoint

--- teachers/urls.py
+++ teachers/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.TeachersViewSet.as_view(), name='teachers'),
+    path('skills', views.SkillsViewSet.as_view(), name='skills'),
 ]
 
--- teachers/views.py
+++ teachers/views.py
@@ -4,7 +4,7 @@ from teachers.serializers import (TeacherSerializer, SkillSerializer)
 
 
 class SkillsViewSet(generics.ListAPIView):
-    queryset = Teacher.objects.all().order_by('name')
+    queryset = Skill.objects.all().order_by('name')
     serializer_class = SkillSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['name']
